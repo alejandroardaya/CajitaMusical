1eef6a49bffbf06b23031739d5396102e157a6be
alejandroardaya <o.a.ardaya.cau@gmail.com>
2025-06-14 09:57:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const mensaje = document.getElementById('mensaje');
 
 btn.addEventListener('click', () => {
   tapa.style.transform = 'rotateX(-90deg)';
-  bailarina.style.animation = 'spin 2s linear infinite';
+  bailarina.style.animationPlayState = 'running';
   audio.play();
-  mensaje.classList.remove('hidden');
+  mensaje.style.display = 'block';
   btn.disabled = true;
 });
